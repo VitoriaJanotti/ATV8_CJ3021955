fdfd3418eb3f0bd3964ded194fbecaa6fe163810
Maria Vitória Janotti <160731952+VitoriaJanotti@users.noreply.github.com>
2024-05-22 10:23:06 -0300

    Update Program.cs

--- ATV8_CJ3021955/Program.cs
+++ ATV8_CJ3021955/Program.cs
@@ -1,70 +1,53 @@
-namespace ATV8_CJ3021955
-{
-    internal class Program
-    {
-        static void Main(string[] args)
+   for (int i = 0;i < 4 ;i++) 
+   {
+        for (int j = 0;j < 4; j++)
         {
-            /*
-            //EXEMPLO DE MATRIZ DADO EM AULA 
-
-            int[,] tab = new int[11, 11];
-
-            for (int i = 0; i <= 10; i++) //percorre linhas
-            {
-                for (int j = 0; j <= 10; j++) //percorre coluna
-                {
-                    tab[i, j] = i * j;
-                }
-            }
-            for (int i = 0; i <= 10; i++) //percorre linhas
-            {
-                for (int j = 0; j <= 10; j++) //percorre coluna
-                {
-                    Console.Write(" {0:00}  ", tab[i, j]);
-                }
-                Console.WriteLine();
-            }
-            */
-
-            // jogo da memória
-
-            int[,] jogo = new int[4, 4];
-
-            //para criar números aleatórios 
-            Random gerador = new Random();
-
-           for(int i = 0; i < 8; i++) //atribui os pares de números ás posições
-           {
-                //Escolhe a posição do primeiro numero do par
-                int lin, col;
-                do
-                {
-                    lin = gerador.Next(0, 4);
-                    col = gerador.Next(0, 4);
-
-                } while (jogo[lin, col] != 0);
-                jogo[lin, col] = i + 1;
-
-                //Escolhe a posição do primeiro numero do par
-                do
-                {
-                    lin = gerador.Next(0, 4);
-                    col = gerador.Next(0, 4);
-
-                } while (jogo[lin, col] != 0);
-                jogo[lin, col] = i + 1;
-            }
-
-           for (int i = 0;i < 4 ;i++) 
-           {
-                for (int j = 0;j < 4; j++)
-                {
-                    Console.Write(" {0}  ", jogo[i, j]);
-                }
-                Console.WriteLine();
-            }
-            
-
+            Console.Write(" {0}  ", tela[i, j]);
         }
+        Console.WriteLine();
     }
+   //pedir as posiç~es do primeiro número 
+    Console.WriteLine("Escolha uma linha para jogar [1, 4]: ");
+    int lin1 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Escolha uma coluna para jogar [1, 4]: ");
+    int col1 = int.Parse(Console.ReadLine());
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (i == lin1 - 1 && j == col1 - 1)
+                Console.Write("{0} ", jogo[i, j]);
+            else
+                Console.Write("{0} ", tela[i, j]);
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+
+    //pedir as posições do segundo número 
+    Console.WriteLine("Escolha uma linha para jogar [1, 4]: ");
+    int lin2 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Escolha uma coluna para jogar [1, 4]: ");
+    int col2 = int.Parse(Console.ReadLine());
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (i == lin2 - 1 && j == col2 - 1)
+                Console.Write("{0} ", jogo[i, j]);
+            else
+                Console.Write("{0} ", tela[i, j]);
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+
+    //em caso de acerto a matriz tela permanece como está
+    //em caso de erro, precisamos voltar as posições para zero 
+    if ()
+
 }

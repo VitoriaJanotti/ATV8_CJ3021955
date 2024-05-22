2c6ebf80510d556faeba4773447b2252fd933ee5
CJ3021955 <CJ3021955@LAB03-PC01>
2024-05-22 08:18:23 -0300

    Adicionar arquivos de projeto.

---
+++ ATV8_CJ3021955/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ATV8_CJ3021955
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+         //EXEMPLO DE MATRIZ DADO EM AULA 
+        {
+            int[,] tab = new int[11, 11];
+
+            for (int i = 0; i <= 10; i++) //percorre linhas
+            {
+                for (int j = 0; j <= 10; j++) //percorre coluna
+                {
+                    tab[i, j] = i * j;
+                }
+            }
+            for (int i = 0; i <= 10; i++) //percorre linhas
+            {
+                for (int j = 0; j <= 10; j++) //percorre coluna
+                {
+                    Console.Write(tab[i, j] );
+                }
+            }
+        }
+    }
+}
